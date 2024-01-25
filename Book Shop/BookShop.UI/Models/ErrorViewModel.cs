911c76b885943379ca1b1d62747d1571f465e1a6
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-01-25 15:18:47 +0700

    Add base source code
    Add source code base NetCore 7.0

---
+++ Book Shop/BookShop.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShop.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
