ac4efb9b0d513a47844b2e8df7e6dad261329688
Đinh Mạnh Tưởng <145707598+prism9x@users.noreply.github.com>
2024-01-25 16:15:48 +0700

    Declare table migration

--- Book Shop/BookShop.DataAccess/DataAccess/ApplicationDbContext.cs
+++ Book Shop/BookShop.DataAccess/DataAccess/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookShop.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookShop.DataAccess.DataAccess
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
@@ -11,9 +13,28 @@ namespace BookShop.DataAccess.DataAccess
 
         }
 
+        // Declare table
+        public DbSet<Catalog> Catalog { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<BookCatalog> BookCatalog { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<CartDetail> CartDetail { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            // AspNetUser rename default of Identity 
+
+            builder.Entity<AppUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
         }
     }
 }
